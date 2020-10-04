c3708c0b501893a5eb2902feabda925ffb0e61ff
Roman <72351742+abirroman143@users.noreply.github.com>
2020-10-04 23:30:50 +0600

    bootstrap

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+	jQuery(".scrollicon").click(function(){
+		
+		jQuery("html").animate({'scrollTop':'0'},1000);	
+		
+		
+	});
+	
+	jQuery(".bar").click(function(){
+		
+		jQuery(".main-menu ul").slideToggle();
+		
+	});
